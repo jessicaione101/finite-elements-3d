28aa253b14bbb5e56c87f87946e534abe40ab3c9
Jessica <jessicaione101@gmail.com>
2021-09-24 16:24:40 -0700

    Done shape functions

--- src/phi_linear_tetrahedron.cpp
+++ src/phi_linear_tetrahedron.cpp
@@ -1,6 +1,11 @@
 #include <phi_linear_tetrahedron.h>
 
 void phi_linear_tetrahedron(Eigen::Vector4d &phi, Eigen::Ref<const Eigen::MatrixXd> V, Eigen::Ref<const Eigen::RowVectorXi> element, Eigen::Ref<const Eigen::Vector3d> x) {
+  Eigen::Matrix3d T;
+  T.col(0) = V.row(element(1)) - V.row(element(0));
+  T.col(1) = V.row(element(2)) - V.row(element(0));
+  T.col(2) = V.row(element(3)) - V.row(element(0));
 
-    
+  phi.tail(3) = T.inverse() * (x - V.row(element(0)).transpose());
+  phi(0) = 1 - phi(1) - phi(2) - phi(3);
 }
