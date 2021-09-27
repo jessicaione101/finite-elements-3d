138b1e27a4e7385dba585bedc177a3bf1d01fe23
Jessica <jessicaione101@gmail.com>
2021-09-27 16:42:46 -0700

    Done Neo-Hookean strain energy

--- src/psi_neo_hookean.cpp
+++ src/psi_neo_hookean.cpp
@@ -1,10 +1,8 @@
 #include <psi_neo_hookean.h>
-#include <dphi_linear_tetrahedron_dX.h>
-#include <iostream>
-void psi_neo_hookean(double &psi, 
-                     Eigen::Ref<const Eigen::Matrix3d> F,
-                     double C, double D) {
 
-    
+void psi_neo_hookean(double &psi, Eigen::Ref<const Eigen::Matrix3d> F, double C, double D) {
+  double J = F.determinant();
+  double tr = (F.transpose() * F).trace();
 
+  psi = C * (tr / pow(J, 2./3) - 3) + D * pow(J-1, 2);
 }
