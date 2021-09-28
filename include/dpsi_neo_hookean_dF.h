ee73056b3503508fb062ef88daf149690a9e6197
Jessica <jessicaione101@gmail.com>
2021-09-27 18:53:41 -0700

    Fix parameter name in gradient of Neo-Hookean strain energy

--- include/dpsi_neo_hookean_dF.h
+++ include/dpsi_neo_hookean_dF.h
@@ -6,5 +6,5 @@
 //  C,D - material parameters for the Neo-Hookean model
 //Output:
 //  psi - the 9x1 gradient of the potential energy wrt to the deformation gradient
-void dpsi_neo_hookean_dF(Eigen::Vector9d &psi, Eigen::Ref<const Eigen::Matrix3d> F, double C, double D);
+void dpsi_neo_hookean_dF(Eigen::Vector9d &dw, Eigen::Ref<const Eigen::Matrix3d> F, double C, double D);
 
