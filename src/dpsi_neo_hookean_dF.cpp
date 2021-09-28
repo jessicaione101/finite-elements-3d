130fbfb1ba5e456f1f36072f0256e7d9e24488fd
Jessica <jessicaione101@gmail.com>
2021-09-28 14:39:47 -0700

    Done gradient of Neo-Hookean strain energy

--- src/dpsi_neo_hookean_dF.cpp
+++ src/dpsi_neo_hookean_dF.cpp
@@ -1,14 +1,23 @@
 #include <dpsi_neo_hookean_dF.h>
 
 void dpsi_neo_hookean_dF(Eigen::Vector9d &dw, Eigen::Ref<const Eigen::Matrix3d> F, double C, double D) {
-  static Eigen::Vector9d F_terms;
+  Eigen::Vector9d F_terms;
   F_terms(0) = F(1, 1) * F(2, 2) - F(1, 2) * F(2, 1);
-//  F_terms(1) = F(, ) * F(, ) - F(, ) * F(, );
-//  F_terms(2) = F(, ) * F(, ) - F(, ) * F(, );
-//  F_terms(3) = F(, ) * F(, ) - F(, ) * F(, );
-//  F_terms(4) = F(, ) * F(, ) - F(, ) * F(, );
-//  F_terms(5) = F(, ) * F(, ) - F(, ) * F(, );
-//  F_terms(6) = F(, ) * F(, ) - F(, ) * F(, );
-//  F_terms(7) = F(, ) * F(, ) - F(, ) * F(, );
-//  F_terms(8) = F(, ) * F(, ) - F(, ) * F(, );
+  F_terms(1) = F(1, 2) * F(2, 0) - F(1, 0) * F(2, 2);
+  F_terms(2) = F(1, 0) * F(2, 1) - F(1, 1) * F(2, 0);
+  F_terms(3) = F(0, 2) * F(2, 1) - F(0, 1) * F(2, 2);
+  F_terms(4) = F(0, 0) * F(2, 2) - F(0, 2) * F(2, 0);
+  F_terms(5) = F(0, 1) * F(2, 0) - F(0, 0) * F(2, 1);
+  F_terms(6) = F(0, 1) * F(1, 2) - F(0, 2) * F(1, 1);
+  F_terms(7) = F(0, 2) * F(1, 0) - F(0, 0) * F(1, 2);
+  F_terms(8) = F(0, 0) * F(1, 1) - F(0, 1) * F(1, 0);
+
+  double J = F.determinant();
+  double tr = (F.transpose() * F).trace();
+  dw = F_terms * (2 * D * (J-1) - 2 * C * tr / (3 * pow(J, 5./3)));
+
+  double term = 2 * C / pow(J, 2./3);
+  dw(0) += term * F(0, 0);
+  dw(4) += term * F(1, 1);
+  dw(8) += term * F(2, 2);
 }
