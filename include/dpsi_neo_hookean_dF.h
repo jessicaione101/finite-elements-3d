fec4a05cab750021182cfdc1d9b06dce54f6b4a2
Jessica <jessicaione101@gmail.com>
2021-09-28 18:25:00 -0700

    Fix parameter names

--- include/dpsi_neo_hookean_dF.h
+++ include/dpsi_neo_hookean_dF.h
@@ -5,6 +5,6 @@
 //  F - the dense 3x3 deformation gradient
 //  C,D - material parameters for the Neo-Hookean model
 //Output:
-//  psi - the 9x1 gradient of the potential energy wrt to the deformation gradient
+//  dw - the 9x1 gradient of the potential energy wrt to the deformation gradient
 void dpsi_neo_hookean_dF(Eigen::Vector9d &dw, Eigen::Ref<const Eigen::Matrix3d> F, double C, double D);
 
