3d03c806a2d2b0b07cd025662d136cf2e9224b40
Jessica <jessicaione101@gmail.com>
2021-10-04 18:05:16 -0700

    Done kinetic energy of a single tetrahedron

--- src/T_linear_tetrahedron.cpp
+++ src/T_linear_tetrahedron.cpp
@@ -1,8 +1,8 @@
 #include <T_linear_tetrahedron.h>
 #include <mass_matrix_linear_tetrahedron.h>
-#include <iostream>
-void T_linear_tetrahedron(double &T, Eigen::Ref<const Eigen::VectorXd> qdot, Eigen::Ref<const Eigen::RowVectorXi> element, double density, double volume) {
 
-    
-        
+void T_linear_tetrahedron(double &T, Eigen::Ref<const Eigen::VectorXd> qdot, Eigen::Ref<const Eigen::RowVectorXi> element, double density, double volume) {
+  Eigen::Matrix1212d M;
+  mass_matrix_linear_tetrahedron(M, qdot, element, density, volume);
+  T = 0.5 * qdot.transpose() * M * qdot;
 }
