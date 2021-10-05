488bb73b86d161cc2a28f48b8142f68f63b41dd9
Jessica <jessicaione101@gmail.com>
2021-10-04 18:48:41 -0700

    Add comment to help understand the parameters in single point quadrature integrand

--- include/quadrature_single_point.h
+++ include/quadrature_single_point.h
@@ -12,7 +12,5 @@ template<typename Ret, typename Integrand_Func>
 inline void quadrature_single_point(Ret &&integrated, Eigen::Ref<const Eigen::VectorXd> q, 
                                                Eigen::Ref<const Eigen::RowVectorXi> element, double volume,
                                                Integrand_Func integrand) {
-
-
+  // X is actually meant to be the barycentric coordinates of the point where the integrand is evaluated
 }
-
