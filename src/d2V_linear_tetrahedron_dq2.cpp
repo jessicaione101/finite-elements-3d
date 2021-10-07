ea75d54b1397d9801ae5b3046e0f307afbf3ae80
Jessica <jessicaione101@gmail.com>
2021-10-06 18:10:12 -0700

    Code style

--- src/d2V_linear_tetrahedron_dq2.cpp
+++ src/d2V_linear_tetrahedron_dq2.cpp
@@ -6,30 +6,27 @@
 void d2V_linear_tetrahedron_dq2(Eigen::Matrix1212d &H, Eigen::Ref<const Eigen::VectorXd> q, 
                           Eigen::Ref<const Eigen::MatrixXd> V, Eigen::Ref<const Eigen::RowVectorXi> element, double volume,
                           double C, double D) {
+  auto neohookean_linear_tet = [&](Eigen::Matrix1212d &dV, Eigen::Ref<const Eigen::VectorXd>q, Eigen::Ref<const Eigen::RowVectorXi> element, Eigen::Ref<const Eigen::Vector3d> X) {
 
-   auto neohookean_linear_tet = [&](Eigen::Matrix1212d &dV, Eigen::Ref<const Eigen::VectorXd>q, Eigen::Ref<const Eigen::RowVectorXi> element, Eigen::Ref<const Eigen::Vector3d> X) {
-        
-      //Code to compute non-integrated hessian matrix goes here
-     
-    };
+    //Code to compute non-integrated hessian matrix goes here
 
-    //integrate the non-integrated hessian across the tetrahedral element
-    quadrature_single_point(H, q, element, volume, neohookean_linear_tet);  
-    
+  };
 
-    //DO NOT REMOVE THIS CODE This code ensures that the hessian matrix is symmetric postive definite by projecting all
-    //negative eigenvalues to small, postive values.
-    Eigen::SelfAdjointEigenSolver<Eigen::Matrix1212d> es(H);
-    
-    Eigen::MatrixXd DiagEval = es.eigenvalues().real().asDiagonal();
-    Eigen::MatrixXd Evec = es.eigenvectors().real();
-    
-    for (int i = 0; i < 12; ++i) {
-        if (es.eigenvalues()[i]<1e-6) {
-            DiagEval(i,i) = 1e-3;
-        }
+  //integrate the non-integrated hessian across the tetrahedral element
+  quadrature_single_point(H, q, element, volume, neohookean_linear_tet);
+
+  //DO NOT REMOVE THIS CODE This code ensures that the hessian matrix is symmetric positive definite by projecting all
+  //negative eigenvalues to small, positive values.
+  Eigen::SelfAdjointEigenSolver<Eigen::Matrix1212d> es(H);
+
+  Eigen::MatrixXd DiagEval = es.eigenvalues().real().asDiagonal();
+  Eigen::MatrixXd Evec = es.eigenvectors().real();
+
+  for (int i = 0; i < 12; ++i) {
+    if (es.eigenvalues()[i]<1e-6) {
+      DiagEval(i,i) = 1e-3;
     }
+  }
 
-    H = Evec * DiagEval * Evec.transpose();
-
+  H = Evec * DiagEval * Evec.transpose();
 }
