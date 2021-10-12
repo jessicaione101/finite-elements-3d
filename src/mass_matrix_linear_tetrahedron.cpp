7bd279dab414c3c73dc941a87c88e2cfb673c39d
Jessica <jessicaione101@gmail.com>
2021-10-11 17:58:51 -0700

    Fix indentation

--- src/mass_matrix_linear_tetrahedron.cpp
+++ src/mass_matrix_linear_tetrahedron.cpp
@@ -1,4 +1,4 @@
-  #include <mass_matrix_linear_tetrahedron.h>
+#include <mass_matrix_linear_tetrahedron.h>
 
  void mass_matrix_linear_tetrahedron(Eigen::Matrix1212d &M, Eigen::Ref<const Eigen::VectorXd> qdot, Eigen::Ref<const Eigen::RowVectorXi> element, double density, double volume) {
   auto I = Eigen::Matrix3d::Identity();
